8a8afcbde969a970c213b1835022e1a44207959c
Yichen <ichisaur@gmail.com>
2018-06-14 02:20:10 -0500

    Bit of tuning

--- Final Project PIC/PIC Controller/firmware/PIC Controller.X/build/default/production/_ext/1360937237/main.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1360937237/main.o:  \
-../src/main.c  \
- ../../../../../../../../../Programs/Harmony/framework/system/common/sys_module.h  \
- ../../../../../../../../../Programs/Harmony/framework/system/common/sys_common.h 
---
+++ MPLAB Sucks/firmware/MPLAB Sucks.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MPLAB_Sucks.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MPLAB_Sucks.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mplabsucks.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mplabsucks.x.tar
